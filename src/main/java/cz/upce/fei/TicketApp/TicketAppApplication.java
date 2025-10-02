75389bfa21a7c1edc755f5a51eab5190472a2126
David Polívka <125467726+st67289@users.noreply.github.com>
2025-10-02 13:03:37 +0200

    Add files via upload

---
+++ src/main/java/cz/upce/fei/TicketApp/TicketAppApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.TicketApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/TicketApp/TicketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.TicketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
