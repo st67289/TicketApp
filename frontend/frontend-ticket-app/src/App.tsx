57a00e3ea9c743e1301edf4797e618723b37192e
Adam Dvořák <st67024@upce.cz>
2025-10-07 15:16:54 +0200

    instalace frontendu

---
+++ frontend/frontend-ticket-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <Routes>
+        TODO routy zde - pro každou cestu určitý soubor
+    </Routes>
+  )
+}
+
+export default App
