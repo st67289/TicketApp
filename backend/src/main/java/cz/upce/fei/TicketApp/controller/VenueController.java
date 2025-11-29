e71f4e76e15a73f3136d464749c94152d85e3e66
Axoden <janousek523@gmail.com>
2025-11-29 17:04:53 +0100

    Odstranění bílých rámečků a code cleanup

--- backend/src/main/java/cz/upce/fei/TicketApp/controller/VenueController.java
+++ backend/src/main/java/cz/upce/fei/TicketApp/controller/VenueController.java
@@ -5,7 +5,6 @@ import cz.upce.fei.TicketApp.dto.venue.VenueDto;
 import cz.upce.fei.TicketApp.service.VenueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
